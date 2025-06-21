2db4b45901f7b5f7eafe98026d35e79a93d964bb
karan819381 <karan819381@gmail.com>
2025-06-21 13:36:06 +0530

    setup vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Card from './components/Card.vue';
+</script>
+
+<template>
+  <main
+    class="mx-auto h-screen w-full max-w-[90rem] bg-[url('/images/bg-pattern.svg')] bg-no-repeat"
+  >
+    <div
+      class="item-center relative place-items-center w-full pt-[4rem] text-center"
+    >
+      <img src="/images/pattern-circles.svg" alt="" class="absolute left-1/2 -translate-x-1/2" />
+      <h1 class="mt-[2.5rem] font-bold text-[1.78rem] text-CTA-bg">Simple, traffic-based pricing</h1>
+      <p class="text-[0.938rem] mt-[0.4rem]">Sign-up for our 30-day trial. No credit card required.</p>
+    </div>
+    <div class="flex justify-center  items-center w-full ">
+      <Card />
+    </div>
+  </main>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-[33.7rem] bg-green-300 mt-[5.6rem] rounded-md px-[2.8rem]">
+        <div class="mt-[2.5rem] flex items-center justify-between">
+            <h2 class="uppercase tracking-[0.1186rem] text-[0.938rem]">100K Pageviews</h2>
+            <p class="text-center"><span class="text-[2.5rem] font-bold">$16.00 </span> / month</p>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
