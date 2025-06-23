efc2c8c351c7b4a687206c637216f3321c478845
karan819381 <karan819381@gmail.com>
2025-06-23 12:51:19 +0530

    desktop design done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Card from './components/Card.vue';
+import Card from "./components/Card.vue";
 </script>
 
 <template>
@@ -7,13 +7,21 @@ import Card from './components/Card.vue';
     class="mx-auto h-screen w-full max-w-[90rem] bg-[url('/images/bg-pattern.svg')] bg-no-repeat"
   >
     <div
-      class="item-center relative place-items-center w-full pt-[4rem] text-center"
+      class="item-center relative w-full place-items-center pt-[4rem] text-center"
     >
-      <img src="/images/pattern-circles.svg" alt="" class="absolute left-1/2 -translate-x-1/2" />
-      <h1 class="mt-[2.5rem] font-bold text-[1.78rem] text-CTA-bg">Simple, traffic-based pricing</h1>
-      <p class="text-[0.938rem] mt-[0.4rem]">Sign-up for our 30-day trial. No credit card required.</p>
+      <img
+        src="/images/pattern-circles.svg"
+        alt=""
+        class="absolute left-1/2 -translate-x-1/2"
+      />
+      <h1 class="text-CTA-bg mt-[2.5rem] text-[1.78rem] font-bold">
+        Simple, traffic-based pricing
+      </h1>
+      <p class="mt-[0.4rem] text-[0.938rem]">
+        Sign-up for our 30-day trial. No credit card required.
+      </p>
     </div>
-    <div class="flex justify-center  items-center w-full ">
+    <div class="flex w-full items-center justify-center">
       <Card />
     </div>
   </main>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,110 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const value = ref(50);
+
+const sliderFill = computed(() => {
+  const percent = ((value.value - 1) / 99) * 100;
+  return `--percent: ${percent}%`;
+});
+</script>
+
 <template>
-    <div class="w-[33.7rem] bg-green-300 mt-[5.6rem] rounded-md px-[2.8rem]">
-        <div class="mt-[2.5rem] flex items-center justify-between">
-            <h2 class="uppercase tracking-[0.1186rem] text-[0.938rem]">100K Pageviews</h2>
-            <p class="text-center"><span class="text-[2.5rem] font-bold">$16.00 </span> / month</p>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
+  <div
+    class="bg-main-bg mt-[5.6rem] w-[33.7rem] rounded-md px-[2.8rem] shadow-lg"
+  >
+    <div class="mt-[2.5rem] flex items-center justify-between">
+      <h2 class="text-[0.938rem] tracking-[0.1186rem] uppercase">
+        100K Pageviews
+      </h2>
+      <p class="text-center">
+        <span class="text-CTA-bg text-[2.5rem] font-bold">$16.00 </span> / month
+      </p>
     </div>
+
+    <div class="mt-[1.7rem]">
+      <input
+        type="range"
+        name=""
+        id=""
+        min="1"
+        max="100"
+        v-model="value"
+        class="custom-slider bg-empty-slidebar h-[0.6rem] w-full appearance-none rounded-xl transition-opacity outline-none"
+        :style="sliderFill"
+      />
+    </div>
+
+    <div class="mt-[3rem] flex items-center justify-center space-x-3 text-sm">
+      <span class="text-xs">Monthly Billing</span>
+
+      <label class="relative inline-flex cursor-pointer items-center">
+        <input type="checkbox" class="peer sr-only" />
+        <div
+          class="peer bg-toggle-bg peer-checked:bg-slider-bg h-5 w-10 rounded-full peer-focus:outline-none after:absolute after:top-[2px] after:left-[3px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all peer-checked:after:translate-x-full peer-checked:after:border-white"
+        ></div>
+      </label>
+
+      <span class="flex items-center gap-1 text-xs">
+        Yearly Billing
+        <span
+          class="bg-discount-bg text-discount-text rounded-full px-2 py-0.5 text-xs font-bold"
+          >25% discount</span
+        >
+      </span>
+    </div>
+
+    <hr class="mt-[2.3rem]" />
+
+    <div class="my-[2rem] flex items-center justify-between">
+      <ul>
+        <li class="text-[0.7rem]">Unlimited websites</li>
+        <li class="my-3 text-[0.7rem]">100% data ownership</li>
+        <li class="text-[0.7rem]">Email reports</li>
+      </ul>
+      <button
+        class="bg-CTA-bg text-CTA-text cursor-pointer rounded-full px-12 py-3 text-xs"
+      >
+        Start my trial
+      </button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.custom-slider::-webkit-slider-thumb {
+  appearance: none;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  background-color: var(--color-slider-bg);
+  background-image: url("/images/icon-slider.svg");
+  background-position: center;
+  background-repeat: no-repeat;
+  cursor: pointer;
+}
+
+li::before {
+  content: "";
+  display: inline-block;
+  margin-right: 0.5rem;
+  background: url("/images/icon-check.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 0.6rem;
+  height: 0.6rem;
+}
+
+/* .custom-slider::-webkit-slider-runnable-track {
+  height: 8px;
+  border-radius: 50%;
+  background: linear-gradient(
+    to right,
+    #2dd4bf 0%,
+    #2dd4bf calc(var(--percent) * 1%),
+    #d1fae5 calc(var(--percent) * 1%),
+    #d1fae5 100%
+  );
+  background-size: 100% 100%;
+} */
+</style>
