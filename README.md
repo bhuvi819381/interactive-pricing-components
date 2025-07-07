971cf31f740963f456f47a1d3aace8cd325bbcb1
karan819381 <karan819381@gmail.com>
2025-07-07 20:20:39 +0530

    complete

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
-# Vue 3 + TypeScript + Vite
+# Frontend Mentor - Interactive pricing component solution
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+This is a solution to the [Interactive pricing component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers
+
+### Screenshot
+
+![](./public/screenshot/image.png)
+
+### Links
+
+- Solution URL: [Solution](https://your-solution-url.com)
+- Live Site URL: [Live](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Desktop-first workflow
+- [Vue](https://vuejs.org/) - JS library
+
+- [tailwindcss](https://tailwindcss.com/) - For styles
+
+## Author
+
+- Frontend Mentor - [@bhuvi819381](https://www.frontendmentor.io/profile/bhuvi819381)
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 
-const value = ref(50);
+const value = ref(2);
+const isYearly = ref(false);
 
-const sliderFill = computed(() => {
-  const percent = ((value.value - 1) / 99) * 100;
-  return `--percent: ${percent}%`;
+const plans = [
+  { views: "10K", price: 8 },
+  { views: "50K", price: 12 },
+  { views: "100K", price: 16 },
+  { views: "500K", price: 24 },
+  { views: "1M", price: 36 },
+];
+
+const currentPlan = computed(() => plans[value.value - 1]);
+
+const finalPrice = computed(() => {
+  const base = currentPlan.value.price;
+  return isYearly.value ? (base * 0.75).toFixed(2) : base.toFixed(2);
 });
+const percent = ((value.value - 1) * 4) / 100;
+
+const sliderStyle = computed(() => ({
+  "--percent": `${percent.valueOf}%`,
+}));
 </script>
 
 <template>
@@ -15,10 +31,13 @@ const sliderFill = computed(() => {
   >
     <div class="mt-[2.5rem] flex items-center justify-between">
       <h2 class="text-[0.938rem] tracking-[0.1186rem] uppercase">
-        100K Pageviews
+        {{ currentPlan.views }} Pageviews
       </h2>
       <p class="text-center">
-        <span class="text-CTA-bg text-[2.5rem] font-bold">$16.00 </span> / month
+        <span class="text-CTA-bg text-[2.5rem] font-bold"
+          >${{ finalPrice }}
+        </span>
+        / {{ isYearly ? "year" : "month" }}
       </p>
     </div>
 
@@ -28,18 +47,19 @@ const sliderFill = computed(() => {
         name=""
         id=""
         min="1"
-        max="100"
+        max="5"
         v-model="value"
         class="custom-slider bg-empty-slidebar h-[0.6rem] w-full appearance-none rounded-xl transition-opacity outline-none"
-        :style="sliderFill"
+        :style="sliderStyle"
       />
+      <!-- bg-empty-slidebar h-[0.6rem] w-full appearance-none rounded-xl transition-opacity outline-none -->
     </div>
 
     <div class="mt-[3rem] flex items-center justify-center space-x-3 text-sm">
       <span class="text-xs">Monthly Billing</span>
 
       <label class="relative inline-flex cursor-pointer items-center">
-        <input type="checkbox" class="peer sr-only" />
+        <input type="checkbox" class="peer sr-only" v-model="isYearly" />
         <div
           class="peer bg-toggle-bg peer-checked:bg-slider-bg h-5 w-10 rounded-full peer-focus:outline-none after:absolute after:top-[2px] after:left-[3px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all peer-checked:after:translate-x-full peer-checked:after:border-white"
         ></div>
@@ -48,6 +68,7 @@ const sliderFill = computed(() => {
       <span class="flex items-center gap-1 text-xs">
         Yearly Billing
         <span
+          v-if="isYearly"
           class="bg-discount-bg text-discount-text rounded-full px-2 py-0.5 text-xs font-bold"
           >25% discount</span
         >
@@ -56,14 +77,14 @@ const sliderFill = computed(() => {
 
     <hr class="mt-[2.3rem]" />
 
-    <div class="my-[2rem] flex items-center justify-between">
+    <div class="my-[2rem] flex items-center justify-between md:flex-row flex-col">
       <ul>
         <li class="text-[0.7rem]">Unlimited websites</li>
         <li class="my-3 text-[0.7rem]">100% data ownership</li>
         <li class="text-[0.7rem]">Email reports</li>
       </ul>
       <button
-        class="bg-CTA-bg text-CTA-text cursor-pointer rounded-full px-12 py-3 text-xs"
+        class="bg-CTA-bg text-CTA-text cursor-pointer rounded-full px-12 py-3 text-xs transition-transform duration-100 active:scale-95 md:mt-0 mt-5"
       >
         Start my trial
       </button>
@@ -72,6 +93,22 @@ const sliderFill = computed(() => {
 </template>
 
 <style scoped>
+/* .custom-slider {
+  appearance: none;
+  width: 100%;
+  height: 8px;
+  border-radius: 9999px;
+  background: linear-gradient(
+    to right,
+    #2dd4bf 0%,
+    #2dd4bf var(--percent),
+    #e5e7eb var(--percent),
+    #e5e7eb 100%
+  );
+  outline: none;
+  transition: background 0.3s;
+} */
+
 .custom-slider::-webkit-slider-thumb {
   appearance: none;
   width: 2.5rem;
@@ -82,6 +119,15 @@ const sliderFill = computed(() => {
   background-position: center;
   background-repeat: no-repeat;
   cursor: pointer;
+  box-shadow: 0 10px 20px var(--color-slider-bg);
+  margin-top: -16px;
+}
+
+.custom-slider::-webkit-slider-runnable-track {
+  height: 8px;
+  border-radius: 50px;
+  /* background: linear-gradient(90deg, red 0%, rgba(255, 0, 0, 0.568) 25% , rgba(255, 255, 0, 0.541) 75%, yellow 100%); */
+  color: #13bba4;
 }
 
 li::before {
@@ -94,17 +140,4 @@ li::before {
   width: 0.6rem;
   height: 0.6rem;
 }
-
-/* .custom-slider::-webkit-slider-runnable-track {
-  height: 8px;
-  border-radius: 50%;
-  background: linear-gradient(
-    to right,
-    #2dd4bf 0%,
-    #2dd4bf calc(var(--percent) * 1%),
-    #d1fae5 calc(var(--percent) * 1%),
-    #d1fae5 100%
-  );
-  background-size: 100% 100%;
-} */
 </style>
